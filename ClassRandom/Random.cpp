4022c1edae5be5b90c90b4f2076e54914c0d0a91
Bocancea Damian <damian.bocancea29@gmail.com>
2026-06-15 18:21:55 +0200

    I added some things

--- ClassRandom/Random.cpp
+++ ClassRandom/Random.cpp
@@ -13,3 +13,4 @@ void Random::getRandomNumber()
             
         return;
 }   
+
--- ClassRandom/Random.hpp
+++ ClassRandom/Random.hpp
@@ -1,11 +1,12 @@
 #ifndef RANDOM_HPP
 #define RANDOM_HPP
 #include <iostream>
-#include "ClassBoard/Board.hpp"
+#include "../ClassBoard/Board.hpp"
 class Random : public Board 
 {
 public :  
     void getRandomNumber() {};
+    
 };
 
 #endif
